e40654849d88649c6b1175555c00776eac601036
delozix <delozix@gmail.com>
2023-03-22 18:44:58 +0300

    Added StringStartsWith solution

---
+++ StringEndsWith/StringStartsWith.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEndsWith
+{
+    internal class StringStartsWith
+    {
+        public static bool Solution(string str, string ending)
+        {
+            if (str.Length < ending.Length)
+            {
+                return false;
+            }
+            return str.Substring(str.Length - ending.Length) == ending;
+        }
+    }
+}
---
+++ StringEndsWith/Test.cs
@@ -0,0 +1,16 @@
+﻿namespace StringEndsWith
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringStartsWith.Solution("sumo", "omo"));
+            Console.WriteLine(StringStartsWith.Solution("samurai", "ai"));
+            Console.WriteLine(StringStartsWith.Solution("abc", "abcd"));
+            Console.WriteLine(StringStartsWith.Solution("abc", "abc"));
+            Console.WriteLine(StringStartsWith.Solution("!@#$%^&*() :-)", ":-)"));
+            Console.WriteLine(StringStartsWith.Solution("abc\n", "abc"));
+
+        }
+    }
+}
