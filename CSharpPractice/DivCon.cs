fc49fc20e175236f3726ee16eb621fd7fe23af2c
delozix <delozix@gmail.com>
2023-03-22 18:35:52 +0300

    Add project files.

---
+++ CSharpPractice/DivCon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    internal class DivCon
+    {
+        public static int DivConq(Object[] objArray)
+        {
+            object str = "str";
+            object num = 1;
+            int strResult = 0;
+            int numResult = 0;
+            for (int i = 0; i < objArray.Length; i++)
+            {
+                if (objArray[i].GetType() == str.GetType())
+                {
+                    strResult += Convert.ToInt32(objArray[i]);
+                }
+                else { numResult += Convert.ToInt32(objArray[i]); }
+            }
+            return (numResult - strResult);
+        }
+    }
+}
---
+++ CSharpPractice/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpPractice
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 == DivCon.DivConq(new object[] { 9, 3, "7", "3" }));
+            Console.WriteLine(-3 == DivCon.DivConq(new object[] {-3, "-2", "2" }));
+            Console.WriteLine(0 == DivCon.DivConq(new object[] { 0, "0" }));
+            Console.WriteLine(-1 == DivCon.DivConq(new object[] { "1" }));
+            Console.WriteLine(28 == DivCon.DivConq(new object[] { 1, 2, 3, 4, 5, 6, 7 }));
+            Console.WriteLine(-28 == DivCon.DivConq(new object[] { "1", "2", "3", "4", "5", "6", "7" }));
+        }
+    }
+}
