0e48debb014e7323e324c86ba029c0a4e1604ca2
delozix <delozix@gmail.com>
2023-03-23 11:39:09 +0300

    initial commit

---
+++ MultipleOf3Or5/MultipleOfTwoNums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleOf3Or5
+{
+    internal class MultipleOfTwoNums
+    {
+        //Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+        //Additionally, if the number is negative, return 0 (for languages that do have them).
+        public static int MultipleOf3or5(int value)
+        {
+            int result = 0;
+            for (int i = 1; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MultipleOf3Or5/test.cs
@@ -0,0 +1,13 @@
+﻿namespace MultipleOf3Or5
+{
+    internal class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(23 == MultipleOfTwoNums.MultipleOf3or5(10));
+            Console.WriteLine(0 == MultipleOfTwoNums.MultipleOf3or5(0));
+            Console.WriteLine(0 == MultipleOfTwoNums.MultipleOf3or5(1));
+            Console.WriteLine(0 == MultipleOfTwoNums.MultipleOf3or5(-10));
+        }
+    }
+}
