d156cdc221866aa13231c327ee3157c5fd5996e4
delozix <delozix@gmail.com>
2023-03-23 11:57:16 +0300

    Added BandNameGenerator tests

---
+++ BandNameGenerator/test.cs
@@ -0,0 +1,13 @@
+﻿namespace BandNameGenerator
+{
+    internal class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Knife" == BandNameGenerator.BandNameGen("knife"));
+            Console.WriteLine("Tartart" == BandNameGenerator.BandNameGen("tart"));
+            Console.WriteLine("Sandlesandles" == BandNameGenerator.BandNameGen("sandles"));
+            Console.WriteLine("The Bed" == BandNameGenerator.BandNameGen("bed"));
+        }
+    }
+}
