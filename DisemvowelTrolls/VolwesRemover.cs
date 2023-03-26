973bd138bc0f4ff5de13ec347a5a665ee3ccad21
delozix <delozix@gmail.com>
2023-03-26 12:49:49 +0300

    initial commit

---
+++ DisemvowelTrolls/VolwesRemover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisemvowelTrolls
+{
+    internal class VolwesRemover
+    {
+        public static string VolwesRemove(string str)
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                bool isVowel = "aeiouAEIOU".IndexOf(str[i]) >= 0;
+                if (isVowel == false)
+                {
+                    result += str[i];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ DisemvowelTrolls/test.cs
@@ -0,0 +1,10 @@
+﻿namespace DisemvowelTrolls
+{
+    internal class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
