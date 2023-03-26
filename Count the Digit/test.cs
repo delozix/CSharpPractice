c872022aa3bb1ce6b322d72c7dc4ba9008fb70da
delozix <delozix@gmail.com>
2023-03-26 12:50:12 +0300

    added count the digit tests

---
+++ Count the Digit/test.cs
@@ -0,0 +1,13 @@
+﻿namespace Count_the_Digit
+{
+    internal class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitsCount.CountTheDigits(5750, 0)); // 4700
+            Console.WriteLine(DigitsCount.CountTheDigits(11549, 1)); // 11905
+            Console.WriteLine(DigitsCount.CountTheDigits(11011, 2)); // 9481
+
+        }
+    }
+}
