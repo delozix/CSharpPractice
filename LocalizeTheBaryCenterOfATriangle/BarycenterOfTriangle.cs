496a3dd50a5b5475496651d931164fcb4c64650f
delozix <delozix@gmail.com>
2023-03-28 18:54:41 +0300

    added BarTriang sln

---
+++ LocalizeTheBaryCenterOfATriangle/BarycenterOfTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalizeTheBaryCenterOfATriangle
+{
+    internal class BarycenterOfTriangle
+    {
+        public static double[] BarTriang(double[] x, double[] y, double[] z)
+        {
+            double[] result = new double[2];
+            for (int i = 0; i < 2; i++)
+            {
+                result[i] = Math.Round(((x[i] + y[i] + z[i]) / 3), 4);
+            }
+            return result;
+        }
+    }
+}
