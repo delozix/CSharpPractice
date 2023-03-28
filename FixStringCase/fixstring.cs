55d9a142f3614fa186ce60b2524a4365625203c9
delozix <delozix@gmail.com>
2023-03-28 18:29:16 +0300

    added fixstringcase sln

---
+++ FixStringCase/fixstring.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixStringCase
+{
+    internal class fixstring
+    {
+        public static string FixStringCase(string s)
+        {
+            int lower = 0;
+            int upper = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == char.ToLower(s[i]))
+                {
+                    lower++;
+                }
+                else { upper++; }
+            }
+            return lower > upper || lower == upper ? s.ToLower() : s.ToUpper();
+        }
+    }
+}
