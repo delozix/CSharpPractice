f2cf49ae62bf34305c14dbcb68dee4d3a68d961c
delozix <delozix@gmail.com>
2023-03-28 18:29:25 +0300

    added tests

---
+++ FixStringCase/tests.cs
@@ -0,0 +1,13 @@
+﻿namespace FixStringCase
+{
+    internal class tests
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fixstring.FixStringCase("case")); // case
+            Console.WriteLine(fixstring.FixStringCase("CAse")); // case
+            Console.WriteLine(fixstring.FixStringCase("CASe")); // CASE
+            Console.WriteLine(fixstring.FixStringCase("Case")); // case
+        }
+    }
+}
